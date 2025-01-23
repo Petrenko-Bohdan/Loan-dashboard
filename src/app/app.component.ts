8b25878ffc8d1ff9cba5a4988bdd844e38dbe39b
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2025-01-23 20:34:28 +0100

    add loan details component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoanDetailsComponent } from './loan-details/loan-details.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoanDetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
