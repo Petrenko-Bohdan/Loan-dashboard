c3a2fd73e129be420ba3cb0f65282b13e80704a3
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2025-01-26 17:16:33 +0100

    Add Loan interface to define the structure of loan data

--- src/app/models/loan.model.ts
+++ src/app/models/loan.model.ts
@@ -1,13 +1,13 @@
 export interface Loan {
-	id: number;
-	amount: number;
-	term: number;
-	rateType: string;
-	interestRate: number;
-	postOverpayment: string;
-	totalPayments: number;
-	totalOverpayment: number;
-	totalCost: number;
-	repaymentTime: number;
-	profit: string;
+  id: number;
+  amount: number;
+  term: number;
+  rateType: string;
+  interestRate: number;
+  postOverpayment: string;
+  totalPayments: number;
+  totalOverpayment: number;
+  totalCost: number;
+  repaymentTime: number;
+  profit: string;
 }
