d937b581b609cf4abaacc08ba1aa5bb551ecb689
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2025-01-26 17:15:52 +0100

    Integrate NgRx state management: Added store, effects, and devtools configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  isDevMode,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+
+import { provideStore } from '@ngrx/store';
+import { loanReducer } from './store/loan.reducer';
+import { LoanEffects } from './store/loan.effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideEffects([LoanEffects]),
+		provideStore({ loans: loanReducer }),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+  ],
 };
