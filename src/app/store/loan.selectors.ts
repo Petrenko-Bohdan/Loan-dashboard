0a29ab5adff34029561d9d4658d31c142542c542
Bohdan Petrenko <bohdan.petrenko.web@gmail.com>
2025-01-30 19:50:22 +0100

    "Refactor: Update NgRx selectors for LoanState
    - Updated `selectLoanState` to use `AppState` for better type safety
    - Ensured `selectPaymentSchedule` returns an empty array if undefined
    - Improves maintainability and prevents potential runtime errors"

--- src/app/store/loan.selectors.ts
+++ src/app/store/loan.selectors.ts
@@ -1,14 +1,10 @@
 import { createSelector, createFeatureSelector } from '@ngrx/store';
 import { LoanState } from './loan.reducer';
+import { AppState } from './app.state';
 
-export const selectLoanState = createFeatureSelector<LoanState>('loan');
+export const selectLoanState = createFeatureSelector<AppState, LoanState>('loans');
 
 export const selectPaymentSchedule = createSelector(
   selectLoanState,
-  (state: LoanState) => state.paymentSchedule
+  (state: LoanState) => state.paymentSchedule || []
 );
-
-export const selectLoanById = (loanId: string) =>
-  createSelector(selectLoanState, (state: LoanState) =>
-    state.loan
-  );
